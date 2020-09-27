f06874db39dfdd6bf696d4a3b3f69214cd1e8d21
Bober2983 <bober2983@gmail.com>
2020-09-27 21:27:33 +0300

    Update #1

--- lessons.py
+++ lessons.py
@@ -6,7 +6,7 @@ from config import write_message_null,con,ch_nch_week
 
 def lessons_p (sender,reseived_message):
 
-    weekly = datetime.datetime.today().strftime("%A") #ДЕНЬ НЕДЕЛИ
+    weekly = datetime.datetime.today().strftime("%A") # ДЕНЬ НЕДЕЛИ
     date = datetime.datetime.today().strftime("%d.%m.%Y") # ДАТА
     time = datetime.datetime.today().strftime("%H:%M:%S") # ВРЕМЯ
 
@@ -17,7 +17,7 @@ def lessons_p (sender,reseived_message):
     write_message_null(sender, '('+date+')' + "    Сегодня у нас: " + weekly + "\nВремя: " + time+'\n' )
     for row in rows:
         check = True
-        text = '****************************************\n'+'Предмет: ' + row[0] + '\n Время пары: ' + row[2] + '\n Преподователь: ' + row[3] + '\n Аудитория: ' + row[4] + '\n Тип пары: ' + row[5] + '\n ****************************************'
+        text = '***************************************\n'+'Предмет: ' + row[0] + '\n Время пары: ' + row[2] + '\n Преподователь: ' + row[3] + '\n Аудитория: ' + row[4] + '\n Тип пары: ' + row[5] + '\n ***************************************'
         write_message_null(sender, text)
     if check == False:
         write_message_null(sender, "Сегодня занятий нет!\nМожешь отдохнуть!")
